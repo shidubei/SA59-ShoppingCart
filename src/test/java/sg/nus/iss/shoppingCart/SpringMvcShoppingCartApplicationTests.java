f5c08eb5fdf79c890f28cbea81b255bad33130d7
ShiDu <1364136725@qq.com>
2024-10-01 15:56:50 +0800

    start coding

---
+++ src/test/java/sg/nus/iss/shoppingCart/SpringMvcShoppingCartApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.nus.iss.shoppingCart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcShoppingCartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
