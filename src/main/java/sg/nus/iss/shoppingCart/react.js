e9f968a12177655b0759d6d805ab4781916a0d19
chingnam <chingnam1234@foxmail.com>
2024-10-08 13:19:01 +0800

    10.8 update chingnam

---
+++ src/main/java/sg/nus/iss/shoppingCart/react.js
@@ -0,0 +1,10 @@
+import React from "react";
+function App() {
+return (
+<div>
+<p>Hello World</p>
+</div>
+);
+}
+export default App;
+
---
+++ src/main/java/sg/nus/iss/shoppingCart/repository/ProductRepository.java
@@ -0,0 +1,12 @@
+//创建存储库
+//ProductRepository
+package com.example.demo.repository;
+
+import com.example.demo.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    // 可以添加自定义查询方法
+}
