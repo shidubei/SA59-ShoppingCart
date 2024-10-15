006e04406c5f936c93ce279a6c733cc49379027c
YasmineK210 <47027575+YasmineK210@users.noreply.github.com>
2024-10-15 11:19:50 +0800

    Update App.js

--- shopping-cart/src/App.js
+++ shopping-cart/src/App.js
@@ -6,13 +6,37 @@ import ShoppingCart from './components/ShoppingCartPage.js'
 import OrderPage from './components/OrderPage.js'
 import ProfilePage from './components/ProfilePage.js'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import axios from 'axios';
+import Appbar from './components/Appbar.js'
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [userData, setUserData] = useState({ shoppingCartItems: 0 });
+  
+  useEffect(()=>{
+    if(isLoggedIn){
+      const fetchUserData = async() => {
+        try{
+          const response = await axios.get('http://localhost:8080/shoppingCart', { withCredentials: true });
+          const shoppingCartItems = response.data.reduce((acc, item) => acc + item.quantity, 0);
+          setUserData({ shoppingCartItems });
+        }
+        catch(error){
+          console.error('Error fetching shopping cart data:', error);
+
+        }
+      };   
+      fetchUserData(); 
+    }
+  }, [isLoggedIn]);
+
   return (
     <BrowserRouter>
+      <Appbar isLoggedIn={isLoggedIn} userData={userData} />
       <Routes>
+        <Route path="/" element={<Login />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<SignUp />} />
+        <Route path="/signup" element={<SignUp />} />
         <Route path="/shoppingCart" element={<ShoppingCart />}/>
         <Route path="/order" element={<OrderPage />}/>
         <Route path="/profile" element={<ProfilePage />}/>
