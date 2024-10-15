05d9bbf3777a78cb42ddae5345b79640998667e5
YasmineK210 <47027575+YasmineK210@users.noreply.github.com>
2024-10-15 14:16:23 +0800

    Update App.js
    Add admin js as one of routes

--- shopping-cart/src/App.js
+++ shopping-cart/src/App.js
@@ -5,45 +5,40 @@ import SignUp from './components/SignUp.js'
 import ShoppingCart from './components/ShoppingCartPage.js'
 import OrderPage from './components/OrderPage.js'
 import ProfilePage from './components/ProfilePage.js'
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import axios from 'axios';
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
 import Appbar from './components/Appbar.js'
+import AdminPage from './components/AdminPage.js'
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const [userData, setUserData] = useState({ shoppingCartItems: 0 });
-  
-  useEffect(()=>{
-    if(isLoggedIn){
-      const fetchUserData = async() => {
-        try{
-          const response = await axios.get('http://localhost:8080/shoppingCart', { withCredentials: true });
-          const shoppingCartItems = response.data.reduce((acc, item) => acc + item.quantity, 0);
-          setUserData({ shoppingCartItems });
-        }
-        catch(error){
-          console.error('Error fetching shopping cart data:', error);
-
-        }
-      };   
-      fetchUserData(); 
-    }
-  }, [isLoggedIn]);
-
   return (
     <BrowserRouter>
-      <Appbar isLoggedIn={isLoggedIn} userData={userData} />
+      <MainLayout />
+    </BrowserRouter>
+  );
+}
+
+function MainLayout() {
+  const location = useLocation();
+  
+  const hideAppbar = location.pathname === '/admin';
+
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [userData, setUserData] = useState({ shoppingCartItems: 0 });
+
+  return (
+    <>
+      {!hideAppbar && <Appbar isLoggedIn={isLoggedIn} userData={userData} />}
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
-        <Route path="/shoppingCart" element={<ShoppingCart />}/>
-        <Route path="/order" element={<OrderPage />}/>
-        <Route path="/profile" element={<ProfilePage />}/>
+        <Route path="/shoppingCart" element={<ShoppingCart />} />
+        <Route path="/order" element={<OrderPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
         <Route path="/home" element={<ProductPage />} />
-    
+        <Route path="/admin" element={<AdminPage />} />
       </Routes>
-    </BrowserRouter>
+    </>
   );
 }
 
